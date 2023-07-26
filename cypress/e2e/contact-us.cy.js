822676287531c187dcacb1cd43276788c3922015
David Guzman <104448135+DavGuzman@users.noreply.github.com>
2023-07-26 17:04:00 +0200

    upload archives

---
+++ cypress/e2e/contact-us.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+//const { describe } = require("mocha");
+
+describe("Test contact us form via WebdriverUni", () => {
+    it("Should be able to submit a succesful submission via contact us form", () => {
+
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        //cy.get('#contact-us').click({force: true});
+        cy.get('[name="first_name"]').type('David');
+        cy.get('[name="last_name"]').type('Guzman');
+        cy.get('[name="email"]').type('David@hotmail.com');
+        cy.get('textarea.feedback-input').type('sadfasdfasdfasdf');
+        cy.get('[type="submit"]').click();
+
+    });
+
+    //it.only para ejecutar solo el test al que se lo pongas
+
+    it("Should not be able to submit a succesful submission via contact us form ass all fields are required", () => {
+
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        cy.get('[name="first_name"]').type('David');
+        cy.get('[name="last_name"]').type('Guzman');
+        cy.get('textarea.feedback-input').type('sadfasdfasdfasdf');
+        cy.get('[type="submit"]').click();
+
+    });
+
+})
+
