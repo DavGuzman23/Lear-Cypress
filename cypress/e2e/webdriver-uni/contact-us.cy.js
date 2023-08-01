c9f31760b70c74493b8a43e55076acffba4b3b11
David Guzman <104448135+DavGuzman@users.noreply.github.com>
2023-08-01 12:17:31 +0200

    upload more asserts

--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -2,9 +2,12 @@
 //const { describe } = require("mocha");
 
 describe("Test contact us form via WebdriverUni", () => {
-    it("Should be able to submit a succesful submission via contact us form", () => {
+    it.only("Should be able to submit a succesful submission via contact us form", () => {
 
         cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+        cy.title().should('include', 'WebDriver | Contact Us');
+        cy.url().should('include', 'contactus');
         //cy.get('#contact-us').click({force: true});
         cy.get('[name="first_name"]').type('David');
         cy.get('[name="last_name"]').type('Guzman');
