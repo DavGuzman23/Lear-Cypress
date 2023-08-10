90c4ae0a94e721427d7531035fbcc2e5d61e95ab
David Guzman <104448135+DavGuzman@users.noreply.github.com>
2023-08-10 14:23:51 +0200

    Promises and closure nested

--- cypress/e2e/automation-test-store/variables-and-cy-commands.cy.js
+++ cypress/e2e/automation-test-store/variables-and-cy-commands.cy.js
@@ -40,5 +40,28 @@ describe("Verifying variables, cypress commands and jquery commands", () => {
 
     });
 
+    it.only("Validate properties of the Contact us page", () => {
+
+        cy.visit('https://automationteststore.com/index.php?rt=content/contact');
+
+        //Uses cypress command and chaining
+        cy.contains('#ContactUsFrm', 'Contact Us Form')
+        .find('#field_11')
+        .should('contain', 'First name')
+
+        //Jquery approach
+        cy.contains('#ContactUsFrm', 'Contact Us Form')
+        .then( text => {
+            const firstNameText = text.find('#field_11').text();
+            expect(firstNameText).to.contain('First name');
+
+            //Embedded commands (Closure)
+            cy.get('#field_11').then(fnText => {
+                cy.log(fnText.text());
+                cy.log(fnText);
+            })
+        });
+
+    });
 
 })
