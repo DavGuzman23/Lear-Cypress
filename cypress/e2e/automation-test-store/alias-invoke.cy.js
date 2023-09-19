3f270edb523759a6e43649c9b5f18c69929dd3cc
DavGuzman23 <GUZMAN.D1@MacBook-Pro-de-David.local>
2023-09-19 16:49:31 +0200

    loop test

--- cypress/e2e/automation-test-store/alias-invoke.cy.js
+++ cypress/e2e/automation-test-store/alias-invoke.cy.js
@@ -32,15 +32,34 @@ describe("Alias and invoke", () => {
         .should('include', 'Add to Cart');
     });
 
-    it.only("Calculate total of normal and sale products", () => {
+    it("Calculate total of normal and sale products", () => {
 
         cy.visit('https://automationteststore.com/');
         cy.get('.thumbnail')
         .as('productlist');
+        /*
         cy.get('@productlist')
         .find('.oneprice')
         .each(($el, index, $list) => {
             cy.log($el.text())
+        })*/
+        cy.get('.thumbnail')
+        .find('.oneprice')
+        .invoke('text')
+        .as('itemPrice');
+        cy.get('@itemPrice')
+        .then($linktext => {
+            var itemPrice = $linktext.split('$');
+            var i;
+            for(i = 0; i < itemPrice.length; i++){
+                /* Interaccion
+                [0] - test
+                [1] - test
+                [2] - test
+                [3] - test
+                [4] - test*/
+                cy.log(itemPrice[i])
+            }
         })
     });
 
