006aac6d0782e50408f3118ef6a04123f00d36c0
DavGuzman23 <gummer2310@gmail.com>
2023-09-27 16:59:29 +0200

    Creacion nuevos test sobre la seguridad

---
+++ cypress/e2e/other/same-origin-policy.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+//const { describe } = require("mocha");
+
+describe("Cypress web security", () => {
+    it("Validate visiting two different domains", () => {
+
+        cy.visit('http://www.webdriveruniversity.com/');
+        cy.visit('https://automationteststore.com/');
+
+    });
+
+    //it.only para ejecutar solo el test al que se lo pongas
+    it("Validate visiting two different domains via user actions", () => {
+
+        cy.visit('http://www.webdriveruniversity.com/');
+        cy.get('#automation-test-store')
+        .invoke('removeAttr', 'target')
+        .click()
+    
+    })
+})
