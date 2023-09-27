a623f215bdb98590c74f39351202bda0aa81ad3b
DavGuzman23 <gummer2310@gmail.com>
2023-09-27 16:44:25 +0200

    Modificacion contact-us test

--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -6,7 +6,10 @@ describe("Test contact us form via WebdriverUni", () => {
 
         //cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
         cy.visit('http://www.webdriveruniversity.com');
-        cy.get('#contact-us').click({force:true})
+        cy.get('#contact-us')
+        //comando jQuery para borrar el atributo y asi al hacer click la pagina no se abre en una nueva ventana.
+        .invoke('removeAttr', 'target')
+        .click({force:true});
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
         cy.title().should('include', 'WebDriver | Contact Us');
         cy.url().should('include', 'contactus');
@@ -25,7 +28,9 @@ describe("Test contact us form via WebdriverUni", () => {
 
         //cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
         cy.visit('http://www.webdriveruniversity.com');
-        cy.get('#contact-us').click({force:true})
+        cy.get('#contact-us')
+        .invoke('removeAttr', 'target')
+        .click({force:true});
         cy.get('[name="first_name"]').type('David');
         cy.get('[name="last_name"]').type('Guzman');
         cy.get('textarea.feedback-input').type('sadfasdfasdfasdf');
