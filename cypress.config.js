4d88b1a4ef6135fe9ae0064568eb66ea5a3672af
DavGuzman23 <gummer2310@gmail.com>
2023-09-27 17:20:41 +0200

    nuevo test con origin y visitas a varias URL's

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specOattern: "cypress/e2e/**/*.{js, jsx, ts, tsx, feature}"
+    specOattern: "cypress/e2e/**/*.{js, jsx, ts, tsx, feature}",
+    chromeWebSecurity: false
   },
 });
--- cypress/e2e/other/same-origin-policy.cy.js
+++ cypress/e2e/other/same-origin-policy.cy.js
@@ -1,21 +1,35 @@
 /// <reference types="Cypress" />
-//const { describe } = require("mocha");
 
 describe("Cypress web security", () => {
-    it("Validate visiting two different domains", () => {
+    it.skip("Validate visiting two different domains", () => {
 
         cy.visit('http://www.webdriveruniversity.com/');
-        cy.visit('https://automationteststore.com/');
+        cy.visit('https://www.google.com');
 
     });
 
-    //it.only para ejecutar solo el test al que se lo pongas
-    it("Validate visiting two different domains via user actions", () => {
+    it.skip("Validate visiting two different domains via user actions", () => {
 
         cy.visit('http://www.webdriveruniversity.com/');
         cy.get('#automation-test-store')
         .invoke('removeAttr', 'target')
         .click()
-    
+        /*
+        .then(() => {
+            expect('#automation-test-store').to.equal('https://automationteststore.com/')
+        })*/
+    })
+
+    it('Origin comand', () => {
+        /*cy.origin('webdriveruniversity.com', () => {
+            cy.visit('/')
+        })
+
+        cy.origin('automationteststore.com', () => {
+            cy.visit('/')
+        })*/
+
+        cy.visit('http://www.webdriveruniversity.com/');
+        cy.visit('http://selectors.webdriveruniversity.com/');
     })
 })
