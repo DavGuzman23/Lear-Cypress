93e9e56339b31aa6ccac77c1eeb070e6e7283b21
DavGuzman23 <gummer2310@gmail.com>
2023-09-27 16:41:12 +0200

    acualizacion de version de cypress

--- cypress/e2e/webdriver-uni/contact-us.cy.js
+++ cypress/e2e/webdriver-uni/contact-us.cy.js
@@ -4,7 +4,9 @@
 describe("Test contact us form via WebdriverUni", () => {
     it("Should be able to submit a succesful submission via contact us form", () => {
 
-        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        //cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        cy.visit('http://www.webdriveruniversity.com');
+        cy.get('#contact-us').click({force:true})
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
         cy.title().should('include', 'WebDriver | Contact Us');
         cy.url().should('include', 'contactus');
@@ -21,7 +23,9 @@ describe("Test contact us form via WebdriverUni", () => {
     //it.only para ejecutar solo el test al que se lo pongas
     it("Should not be able to submit a succesful submission via contact us form ass all fields are required", () => {
 
-        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        //cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html');
+        cy.visit('http://www.webdriveruniversity.com');
+        cy.get('#contact-us').click({force:true})
         cy.get('[name="first_name"]').type('David');
         cy.get('[name="last_name"]').type('Guzman');
         cy.get('textarea.feedback-input').type('sadfasdfasdfasdf');
